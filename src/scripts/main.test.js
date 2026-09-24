146b2e2f5734e88e66626662b78637cc8b8ee26f
blackstrype <blackstrype@gmail.com>
2026-09-24 12:46:39 +0200

    Quick fix of failing tests. TODO: no more evals in vitest setup...

--- src/scripts/main.test.js
+++ src/scripts/main.test.js
@@ -24,7 +24,7 @@ describe('openContactModal', () => {
     `;
 
     // Execute script logic inside the vitest environment
-    eval(scriptContent);
+    eval(scriptContent.replace(/export const openCalendly/g, 'const openCalendly'));
 
     // Dispatch DOMContentLoaded for event listeners
     document.dispatchEvent(new Event('DOMContentLoaded'));
